62e8cc389dda0a1d1ea3a92a41de1fc85a73b299
Obi-Stan-Kenobi <61793167+Obi-Stan-Kenobi@users.noreply.github.com>
2025-09-03 14:32:10 +0200

    Update Code.py
    structural test

--- Code.py
+++ Code.py
@@ -3,3 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 import panda as pd
+
+#Place for defining variables/parameters
+
+
+#Place for functions, make sure to explain them
+
+
+#Place for importing of data
+
+
+#Place for general code
+
+
+#Place for figure generation
+
+
+#Place for saving/ exporting data
